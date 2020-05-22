92027dd1114a8b4a9b95dc1bca0331bd135062c9
artvinicius <arthurviny1999@gmail.com>
2020-05-22 17:07:38 -0300

    Refatore API com CRUD incluindo dados no Array

--- ApiCrudP2.rb
+++ ApiCrudP2.rb
@@ -1,35 +1,60 @@
 require 'sinatra'
 require 'date'
+require 'time'
 
 
-get '/empresa:cnpj' do |cnpj|
-    "
-     <p>Consultando CNPJ: #{cnpj} em #{Date.today}</p>
-    "
+listacnpjs = []
+
+get '/api/empresas' do 
+    if  listacnpjs.size == 0
+        status 404 
+    else
+        "Empresas existente na base: #{listacnpjs} em #{Time.now}"
+    end
 end
 
-post '/empresa' do 
+
+get '/api/empresas/:cnpj' do |cnpj|
+    cnpjEmpresar = params['cnpj']
+    if listacnpjs.include?(cnpjEmpresar)
+    " CNPJ: #{cnpjEmpresar} existente na base em #{Time.now}"
+    else
+        #NOT FOULD
+      status 404
+    end
+end
+
+
+post '/api/empresas/:cnpj' do |cnpj| 
+    if (listacnpjs.include?(cnpj)) then
+        status 409
+    else
+     "
+      Incluindo Empresa  #{cnpj} em #{Time.now}
+     "
+     listacnpjs << cnpj
+     response.headers['Location'] = "http://localhost:4567/empresas/#{cnpj}"
+     status 201
+    end
+end
+
+
+
+put '/api/empresas' do 
     "
-     <p>Inserindo Empresa em #{Date.today}</p>
+     Atualizando Trabalhador  em #{Time.now}
     
     "
 end
 
-put '/empresa:cnpj' do 
+delete '/api/empresas/:cnpj' do 
     "
-     <p>Atualizando Empresa em #{Date.today}</p>
+     Deletando Empresa  em #{Time.now}
     
     "
 end
 
-delete '/empresa:cnpj' do 
-    "
-     <p>Deletando Empresa em #{Date.today}</p>
-    
-    "
-end
-
-#curl http://localhost:4567/empresa:1
-#curl http://localhost:4567/empresa:1 -i -X POST -d '  '
-#curl http://localhost:4567/empresa:1 -i -X PUT -d '  '
-#curl http://localhost:4567/empresa:1 -X DELETE
+# GET >  curl localhost:4567/api/empresas -i
+# GET por CNPJ > curl localhost:4567/api/empresas/111 -i
+# POST > curl localhost:4567/api/empresas/111 -i -X POST -d ' { } '
+# DELETE > curl http://localhost:4567/api/empresas/123456789101 -i -X DELETE
